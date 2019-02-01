b4e5d29d2cb5511a3a0cb1a178413545f28a49f3
qiaogang <qiaogang2003@163.com>
2019-02-01 15:43:29 +0800

    Add test spec

--- test/eventemitter.spec.js
+++ test/eventemitter.spec.js
@@ -18,15 +18,15 @@ jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
 describe('EventEmitter Class test', function () {
 
     it('on()/off() single event type ...ok', function () {
-        let ee = new EventEmitter();
-        let eventName_1 = 'name';
-        let eventName_2 = 'name_2';
+        var ee = new EventEmitter();
+        var eventName_1 = 'name';
+        var eventName_2 = 'name_2';
 
-        ee.on(eventName_1, (ev) => {
+        ee.on(eventName_1, ev => {
             expect(ev.target).toBe(ee);
             expect(ev.args).toBe(1);
 
-        }).on(eventName_2, (ev) => {
+        }).on(eventName_2, ev => {
             expect(ev.target).toBe(ee);
             expect(ev.args).toBe(2);
 
@@ -51,10 +51,10 @@ describe('EventEmitter Class test', function () {
     });
 
     it('on()/off() multi events type ...ok', function () {
-        let ee = new EventEmitter();
-        let eventNames = ' a   b  c  ';
+        var ee = new EventEmitter();
+        var eventNames = ' a   b  c  ';
 
-        ee.on(eventNames, (ev) => {
+        ee.on(eventNames, ev => {
             expect(ev.target).toBe(ee);
             expect(ev.args).toBe(1);
         })
@@ -86,11 +86,60 @@ describe('EventEmitter Class test', function () {
 
     });
 
+    it('once() ...ok', function () {
+        var ee = new EventEmitter();
+        var eventName = 'name';
+
+        var count = 0;
+        ee.once(eventName, ev => {
+            count++;
+            expect(ev.target).toBe(ee);
+            expect(ev.args).toBe(1);
+            expect(count).toBe(1);
+        });
+
+        // emit twice
+        ee.emit(eventName, {
+            args: 1
+        });
+        ee.emit(eventName, {
+            args: 1
+        });
+    });
+
+    it('on() bind content ...ok', function () {
+        var ee = new EventEmitter();
+        ee._sign = 'mask';
+        var evName = 'name';
+
+        ee.on(evName, function (ev) {
+            expect(this._sign).toBe('mask');
+            expect(ev.args).toBe(1);
+        });
+
+        ee.emit(evName, {
+            args: 1
+        });
+
+        var ee1 = new EventEmitter();
+        ee1._sign = 'mask';
+
+        ee1.on(evName, function (ev) {
+            expect(this._sign).not.toBe('mask');
+            expect(this.document).toBe(window.document);
+            expect(ev.args).toBe(1);
+        }, window);
+
+        ee1.emit(evName, {
+            args: 1
+        });
+    });
+
     it('addEventParent()/removeEventParent() ...ok', function () {
-        let ee = new EventEmitter();
-        let parent_1 = new EventEmitter();
-        let parent_2 = new EventEmitter();
-        let eventName = 'name';
+        var ee = new EventEmitter();
+        var parent_1 = new EventEmitter();
+        var parent_2 = new EventEmitter();
+        var eventName = 'name';
 
         ee.addEventParent(parent_1)
           .addEventParent(parent_2);
@@ -100,13 +149,13 @@ describe('EventEmitter Class test', function () {
             expect(ev.args).toBe(1);
         });
 
-        let m = 1;
+        var m = 1;
         parent_2.on(eventName, ev => {
             expect(ev.target).toBe(parent_2);
             expect(ev.args).toBe(m++);
         });
 
-        ee.on(eventName, (ev) => {
+        ee.on(eventName, ev => {
             expect(ev.target).toBe(ee);
             expect(ev.args).toBe(1);
         });
@@ -127,4 +176,75 @@ describe('EventEmitter Class test', function () {
 
     });
 
+    it('addEventParent() set fn ...ok', function () {
+        var ee = new EventEmitter();
+        var parent_1 = new EventEmitter();
+        var evName = 'name';
+
+        parent_1.on(evName, ev => {
+            expect(ev.target).toBe(parent_1);
+            expect(ev.args).toBe(1);
+            expect(ev._sign).toBe('mask');
+        });
+
+        ee.on(evName, ev => {
+            expect(ev.args).toBe(1);
+        });
+
+        ee.addEventParent(parent_1, ev => {
+            ev._sign = 'mask';
+            return ev;
+        });
+
+        ee.emit(evName, {
+            args: 1
+        }, true);
+    });
+
+    it('addEventParent() link three EE ...ok', function () {
+        var son = new EventEmitter();
+        var father = new EventEmitter();
+        var grandpa = new EventEmitter();
+
+        var evName = 'name';
+
+        son.addEventParent(father);
+        father.addEventParent(grandpa);
+
+        grandpa.on(evName, ev => {
+            expect(ev.propagatedFrom).toBe(son);
+            expect(ev.target).toBe(grandpa);
+            expect(ev.args).toBe(1);
+        });
+
+        son.emit(evName, {
+            args: 1
+        }, true);
+    });
+
+    it('emit() stop propagation ...ok', function () {
+        var ee = new EventEmitter();
+        var father = new EventEmitter();
+
+        var evName = 'name';
+        var evName2 = 'notPropagate';
+
+        ee.addEventParent(father);
+        father.on(evName, ev => {
+            expect(ev.args).toBe(1);
+        });
+
+        father.on(evName2, ev => {
+            expect(ev.args).toBe(2);
+        });
+
+        ee.emit(evName, {
+            args: 1
+        }, true);
+
+        ee.emit(evName2, {
+            args: 1
+        });
+
+    })
 });
